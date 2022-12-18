5ad43e45d7aa56aff221754fbe59731f1cacc13b
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-12-18 19:37:31 +0100

    Updates

---
+++ precache-manifest.b40aec4d2b36bdcda5f5c7433b821a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516704972d4334ad15ac266e6544f80d",
+    "url": "/SOOCaml-frontend/index.html"
+  },
+  {
+    "revision": "d946680c26e32f9db6b0",
+    "url": "/SOOCaml-frontend/static/css/2.7a7d2dfe.chunk.css"
+  },
+  {
+    "revision": "a268b2b45e18757c95be",
+    "url": "/SOOCaml-frontend/static/css/main.092e11c7.chunk.css"
+  },
+  {
+    "revision": "d946680c26e32f9db6b0",
+    "url": "/SOOCaml-frontend/static/js/2.b2e12971.chunk.js"
+  },
+  {
+    "revision": "e0cc419307a21d9d8b13d09980919fa8",
+    "url": "/SOOCaml-frontend/static/js/2.b2e12971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a268b2b45e18757c95be",
+    "url": "/SOOCaml-frontend/static/js/main.0cf2ef1e.chunk.js"
+  },
+  {
+    "revision": "965e87059617d4ac0d05",
+    "url": "/SOOCaml-frontend/static/js/runtime-main.f8271a27.js"
+  }
+]);
